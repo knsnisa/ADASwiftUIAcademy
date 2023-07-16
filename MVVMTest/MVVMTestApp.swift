558f973022c1dfa7ef506ed01b5b2dceda9d2fc6
tafani <ratafani>
2023-07-16 23:07:12 +0700

    Initial Commit

---
+++ MVVMTest/MVVMTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMTestApp.swift
+//  MVVMTest
+//
+//  Created by Muhammad Tafani Rabbani on 16/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVMTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
